07de2f2d7cfc559a56b8937ca2dcf808d9ef0416
Paul Bovis <paulbovis@Pauls-MacBook-Pro.local>
2018-04-02 07:45:49 -0600

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,20 @@ const expect  = require("expect"),
 let { app }  = require("./../server"),
 	{ Todo } = require("./../models/Todo");
 
+const todos = [
+	{
+		text: "First test todo"
+	},
+	{
+		text: "Second test todo"
+	}
+];
+
 beforeEach((done) => {
 	Todo.remove({})
+		.then(() => {
+			return Todo.insertMany(todos)
+		})
 		.then(() => done());
 });
 
@@ -25,7 +37,7 @@ describe("POST /todos", () => {
 					return done(err);
 				}
 
-				Todo.find()
+				Todo.find({text})
 					.then((todos) => {
 						expect(todos.length).toBe(1);
 
@@ -53,7 +65,7 @@ describe("POST /todos", () => {
 
 				Todo.find()
 					.then((todos) => {
-						expect(todos.length).toBe(0);
+						expect(todos.length).toBe(2);
 
 						done();
 					})
@@ -61,5 +73,17 @@ describe("POST /todos", () => {
 						done();
 					});
 			});
+	});
+
+	describe ("GET /todos", () => {
+		it ("should get all todos", (done) => {
+			request(app)
+				.get("/todos")
+				.expect(200)
+				.expect((res) => {
+					expect(res.body.todos.length).toBe(2);
+				})
+				.end(done);
+		});
 	});
 });
