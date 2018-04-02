08cc791d5716a3d86df290e3cbdfa69e77412629
Paul Bovis <paulbovis@Pauls-MacBook-Pro.local>
2018-04-02 17:18:08 -0600

    Fix mongodb url 1

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://todouser:DebriS979@ds229609.mlab.com:29609/complete-node-todo");
+mongoose.connect(
+	process.env.MONGODB_URI || "mongodb://todouser:DebriS979@ds229609.mlab.com:29609/complete-node-todo"
+);
 
 module.exports = { mongoose };
