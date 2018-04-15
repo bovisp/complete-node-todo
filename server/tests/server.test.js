c39659fb153ccfd6f94b37c996500cd85f099f40
Paul Bovis <paulbovis@Pauls-MacBook-Pro.local>
2018-04-15 12:56:36 -0600

    Add DELETE /users/me/token test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -313,4 +313,26 @@ describe("POST /todos", () => {
 				});
 		});
 	});
+
+	describe ("DELETE /users/me/token", () => {
+		it ("should remove auth token on logout", (done) => {
+			request(app)
+				.delete("/users/me/token")
+				.set("x-auth", users[0].tokens[0].token)
+				.expect(200)
+				.end((err, res) => {
+					if (err) {
+						done(err);
+					}
+					
+					User.findById(users[0]._id)
+						.then((user) => {
+							expect(user.tokens.length).toBe(0);
+							
+							done();
+						})
+						.catch((e) => done(e));
+				});
+		});
+	});
 });
